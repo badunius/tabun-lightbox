ba86cb8ed735d6505f099c1c88053fe880392ce9
badunius <42402741+badunius@users.noreply.github.com>
2020-10-07 02:26:48 +1000

    Update lightbox.user.js
    better target detection, the script runs before images load

--- lightbox.user.js
+++ lightbox.user.js
@@ -8,71 +8,72 @@
 // @grant        none
 // @downloadURL  https://github.com/badunius/tabun-lightbox/raw/main/lightbox.user.js
 // @updateURL    https://github.com/badunius/tabun-lightbox/raw/main/lightbox.user.js
+// @run-at       document-end
 // ==/UserScript==
 
 
-  const rule = [
-    'img { max-height: 50vh;  }',
-    'img.full { max-height: 92vh; max-width: 92vw; cursor: zoom-out; }',
-    'div.lightbox {position: fixed; background: #333a; width: 100vw; height: 100vh; top: 0; left: 0; z-index: 1000; display: grid; place-content: center; }',
-  ]
-  const style = document.createElement('style')
-	document.body.appendChild(style)
-  rule.forEach(item => style.sheet.insertRule(item))
-  // console.log(style)
-  
-  const cel = (tag, parent) => {
-    const el = document.createElement(tag)
-    const host = !!parent
-    	? parent
-    	: document.body
-    host.appendChild(el)
-    return el
+const rule = [
+  'img { max-height: 50vh;  }',
+  'img.full { max-height: 92vh; max-width: 92vw; cursor: zoom-out; }',
+  'div.lightbox {position: fixed; background: #333a; width: 100vw; height: 100vh; top: 0; left: 0; z-index: 1000; display: grid; place-content: center; }',
+]
+const style = document.createElement('style')
+document.body.appendChild(style)
+rule.forEach(item => style.sheet.insertRule(item))
+// console.log(style)
+
+const cel = (tag, parent) => {
+  const el = document.createElement(tag)
+  const host = !!parent
+  ? parent
+  : document.body
+  host.appendChild(el)
+  return el
+}
+
+const isResized = (img) => {
+  const {
+    clientHeight,
+    naturalHeight
+  } = img
+  return clientHeight !== naturalHeight
+}
+
+isTarget = (element) => {
+  return element.matches('.text img') && !element.matches('.spoiler-title img')
+}
+
+const showLightbox = (img) => {
+  const host = cel('div')
+  host.className = 'lightbox'
+  host.onclick = () => host.remove()
+
+  const zoomed = cel('img', host)
+  zoomed.className = 'full'
+  zoomed.src = img.src
+  zoomed.style.cursor = 'zoom-out'
+
+  console.log(host)
+}
+
+onHover = (evt) => {
+  const { target } = evt
+  if (!isTarget(target)) return
+
+  target.style.cursor = isResized(target)
+    ? 'zoom-in'
+  : 'inherited'
+}
+
+onClick = (evt) => {
+  const { target } = evt
+  if (!isTarget(target)) return
+
+  if (isResized(target)) {
+    console.log('ZOOMING')
+    showLightbox(target)
   }
-  
-  const isResized = (img) => {
-    const {
-        clientHeight,
-        naturalHeight
-      } = img
-    return clientHeight !== naturalHeight
-  }
-  
-  isTarget = (element) => {
-    return element.tagName === 'IMG' && !element.classList.contains('full')
-  }
-  
-  const showLightbox = (img) => {
-    const host = cel('div')
-    host.className = 'lightbox'
-    host.onclick = () => host.remove()
-    
-    const zoomed = cel('img', host)
-    zoomed.className = 'full'
-    zoomed.src = img.src
-    zoomed.style.cursor = 'zoom-out'
-    
-    console.log(host)
-  }
-  
-  onHover = (evt) => {
-    const { target } = evt
-    if (!isTarget(target)) return
-    
-    target.style.cursor = isResized(target)
-    	? 'zoom-in'
-    	: 'inherited'
-  }
-  
-  onClick = (evt) => {
-    const { target } = evt
-    if (!isTarget(target)) return
-    
-    if (isResized(target)) {
-      console.log('ZOOMING')
-      showLightbox(target)
-    }
-  }
-  
-  document.addEventListener('mouseover', onHover)
-  document.addEventListener('click', onClick)
+}
+
+document.addEventListener('mouseover', onHover)
+document.addEventListener('click', onClick)
