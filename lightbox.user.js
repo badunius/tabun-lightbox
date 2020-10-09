892d4f88137b5c265a727acc35cfa2c84a778b83
badunius <42402741+badunius@users.noreply.github.com>
2020-10-10 05:03:33 +1000

    Update lightbox.user.js
    scroll through all images + origin link

--- lightbox.user.js
+++ lightbox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tabun Lightbox
 // @include      https://tabun.everypony.*
-// @version      0.2.0
+// @version      0.2.1
 // @description  limit images size to 50vh, adds lightbox
 // @author       badunius
 // @match        https://tampermonkey.net/index.php?version=4.8&ext=dhdg&updated=true
@@ -11,12 +11,27 @@
 // @run-at       document-end
 // ==/UserScript==
 
+const app = {
+	title: null,
+  current: null,
+  image: null,
+  origin: null,
+  next: null,
+  prev: null,
+}
 
 const rule = [
   '.text img { max-height: 50vh; }',
+  '.text img[width] { max-height: unset; }',
   '.text a img { max-height: initial; }',
   'img.full { max-height: 92vh; max-width: 92vw; cursor: zoom-out; }',
-  'div.lightbox {position: fixed; background: #333a; width: 100vw; height: 100vh; top: 0; left: 0; z-index: 1000; display: grid; place-content: center; }',
+  'div.lightbox {position: fixed; background: #333a; width: 100%; height: 100vh; top: 0; left: 0; z-index: 1000; display: grid; grid-template-columns: 4vmin auto 4vmin; color: #fff; }',
+  'div.lightbox .side { font-size: 4vmin; font-weight: 900; cursor: pointer; user-select: none; }',
+  'div.lightbox .side:hover { background: #fff6; text-shadow: 0 0 2vmin #000; }',
+  'div.lightbox .main { font-size: 2vmin; font-weight: 500; display: grid; grid-template-rows: 4vmin auto 4vmin; }',
+  'div.lightbox .main { font-size: 2vmin; font-weight: 500; display: grid; grid-template-rows: 4vmin auto 4vmin; }',
+  'div.lightbox .item { display: grid; place-content: center; }',
+  'div.lightbox a.item { color: #fff; }',
 ]
 const style = document.createElement('style')
 document.body.appendChild(style)
@@ -40,21 +55,110 @@ const isResized = (img) => {
   return clientHeight !== naturalHeight
 }
 
-isTarget = (element) => {
+const isTarget = (element) => {
   return element.matches('.text img') && !element.matches('.spoiler-title img') && !element.matches('a img')
 }
+
+const findImages = () => [...(document.querySelectorAll('.text img'))]
+const currentIndex = (list = findImages()) => list.findIndex(img => img === app.current)
+
+const onUpdate = () => {
+	app.image.src = app.current.src
+  
+  // title
+  const list = findImages()
+  const index = currentIndex(list)
+  app.title.textContent = `${index + 1}/${list.length}`
+  
+  // origin link
+  const {
+    naturalWidth,
+    naturalHeight
+  } = app.current
+  app.origin.innerHTML = `${naturalWidth}&times;${naturalHeight}`
+  app.origin.href = app.current.src
+}
+
+const onNext = () => {
+	const list = findImages()
+  let index = currentIndex(list)
+  index = (index + 1 + list.length) % list.length
+  app.current = list[index]
+  onUpdate()
+}
+
+const onPrev = () => {
+	const list = findImages()
+  let index = currentIndex(list)
+  index = (index - 1 + list.length) % list.length
+  app.current = list[index]
+  onUpdate()
+}
+
+const onScroll = (evt) => {
+  	evt.preventDefault()
+    const { deltaY = 0 } = evt
+    
+    switch(true) {
+      case deltaY > 0: {
+        onNext()
+        return
+      }
+      case deltaY < 0: {
+        onPrev()
+        return
+      }
+    }
+  }
 
 const showLightbox = (img) => {
   const host = cel('div')
   host.className = 'lightbox'
-  host.onclick = () => host.remove()
+  // host.onclick = () => host.remove()
 
-  const zoomed = cel('img', host)
+  const prev = cel('div', host)
+  prev.innerHTML = '&lang;'
+  prev.className = 'side item'
+  prev.onclick = onPrev
+  
+  const main = cel('div', host)
+  main.className = 'main'
+  
+  const next = cel('div', host)
+  next.innerHTML = '&rang;'
+  next.className = 'side item'
+  next.onclick = onNext
+  
+  
+  const title = cel('div', main);
+  title.className = 'item'
+  title.textContent = '7/12'
+  
+  const container = cel('div', main)
+  container.className = 'item'
+  container.onwheel = onScroll
+  
+  const zoomed = cel('img', container)
   zoomed.className = 'full'
   zoomed.src = img.src
   zoomed.style.cursor = 'zoom-out'
+  zoomed.onclick = () => host.remove()
+  
+  const original = cel('a', main)
+  original.className = 'item'
+  original.textContent = 'link'
+  original.setAttribute('target', '_blank')
+  
+  app.title = title
+  app.current = img
+  app.image = zoomed
+  app.origin = original
+  app.next = next
+  app.prev = prev
 
-  console.log(host)
+  onUpdate()
+  
+  console.log(app)
 }
 
 onHover = (evt) => {
@@ -63,7 +167,7 @@ onHover = (evt) => {
 
   target.style.cursor = isResized(target)
     ? 'zoom-in'
-  : 'inherited'
+  	: 'inherited'
 }
 
 onClick = (evt) => {
@@ -71,7 +175,6 @@ onClick = (evt) => {
   if (!isTarget(target)) return
 
   if (isResized(target)) {
-    console.log('ZOOMING')
     showLightbox(target)
   }
 }
